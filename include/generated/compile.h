9282bb622eadfbde46b0ac7cb8de15afac4e4f71
santod <santod040@gmail.com>
2015-05-29 19:29:29 -0700

    initial 5.1 commit

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 3 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#3 SMP PREEMPT Sat Feb 7 17:41:57 PST 2015"
+#define LINUX_COMPILE_BY "santod"
+#define LINUX_COMPILE_HOST "DevBoX"
+#define LINUX_COMPILER "gcc version 4.9.1 20140602 (prerelease) (GCC) "
---
+++ include/generated/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "3.4.10-NuK3rn3l-m7-Lollipop_v7.00"
